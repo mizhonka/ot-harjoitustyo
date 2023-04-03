b99740bb8348235c6257c952822e135f2b632d2c
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-04-03 10:48:01 +0300

    Yksi testi

--- miinaharava/src/tests/miinaharava_test.py
+++ miinaharava/src/tests/miinaharava_test.py
@@ -1,1 +1,13 @@
 import unittest 
+import pygame
+from level import Level
+
+class TestLevel(unittest.TestCase):
+    def test_grid_parameters(self):
+        x=9
+        y=9
+        m=10
+        level=Level(x, y, m)
+        self.assertEqual(x, level.sizeX)
+        self.assertEqual(y, level.sizeY)
+        self.assertEqual(m, level.mineX)
