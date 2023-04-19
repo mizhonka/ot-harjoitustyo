10a8d8d0f577911710b1f8560c04cd20a3048ca8
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-04-19 16:54:20 +0300

    faf test

--- miinaharava/src/level.py
+++ miinaharava/src/level.py
@@ -15,6 +15,9 @@ from sprites.flag import Flag
 from sprites.win import Win
 from sprites.lose import Lose
 
+# jotain kommentt testtt
+
+
 
 class Level:
     def __init__(self, size_x, size_y, mine_x):
